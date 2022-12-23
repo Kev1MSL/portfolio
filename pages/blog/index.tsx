cb6ade225476a67bf1fdfa64d774d0c7efd27e7c
Kevin Messali <kevinmessali06@gmail.com>
2022-12-23 13:49:16 +0100

    Fixed project page

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -10,19 +10,21 @@ import "@uiw/react-markdown-preview/markdown.css";
 import dynamic from "next/dynamic";
 import {useState} from "react";
 
+import styles from "../../styles/Blog/Blog.module.css";
+
 const MDEditor = dynamic(() => import("@uiw/react-md-editor"), { ssr: false });
 
 export default function Blog() {
     const [value, setValue] = useState<any>("**Hello world!!!**");
     return (
-        <>
+        <div className={styles.container}>
             <Head>
                 <title>Blog | Kevin Messali</title>
             </Head>
             <Navbar />
-            <main>
+            <main className={styles.content}>
                 <div className={"text-center mt-10"}>Currently in development.</div>
             </main>
-        </>
+        </div>
     );
 }
